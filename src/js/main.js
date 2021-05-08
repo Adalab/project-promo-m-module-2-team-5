caeefa6e311f8060311df767dcd2b4503ae854ab
lorena <lorena@MacBook-Pro-de-lorena.local>
2021-05-08 16:03:41 +0200

    js-name

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+//input en el formulario
+const cardFullname = document.querySelector('.form__fill__input');
+//campo card preview
+let previewName = document.querySelector('.card__name');
+
+function contentCard(event) {
+    let content = event.currentTarget.value;
+    previewName.innerHTML = content; 
+};
+//escucho input
+cardFullname.addEventListener('change', contentCard);
