4ba10754c44402b6f858c4f25a0eca6fd63887d0
lorena <lorena@MacBook-Pro-de-lorena.local>
2021-05-09 21:33:53 +0200

    collapsable color

--- src/js/partials/01_preview.js
+++ src/js/partials/01_preview.js
@@ -11,3 +11,21 @@ function contentCard(event) {
 };
 //escucho input
 cardFullname.addEventListener('change', contentCard);
+
+
+const contentForm = document.querySelector('.content__form');
+const formTitle = document.querySelector('.form__title');
+const colorSelection = document.querySelector('.colorselection');
+const fillContent = document.querySelector('.form__fill__content');
+
+function collapsable(event) {
+    let form = event.currentTarget
+    colorSelection.classList.toggle('collapsable_none') = form;
+}
+formTitle.addEventListener('click', collapsable);
+
+
+
+
+
+    
