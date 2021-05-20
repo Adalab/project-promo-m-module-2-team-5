e047f6d0013b6650c8a8dc6be16810de729a0d35
parnasos <lauragraizer@gmail.com>
2021-05-20 19:39:53 +0200

    merge

---
+++ src/js/06_send.js
@@ -0,0 +1,72 @@
+const createButton = document.querySelector(".js-buttonCreateCard");
+const responseElement = document.querySelector(".js-response");
+const twitterContainer = document.querySelector(".js-hidden-collapsable");
+
+function handleClickCreate(event) {
+  // event.preventDefault();
+
+  if (data.name === "") {
+    responseElement.innerHTML = "Recuerda completar el campo 'Nombre'.";
+    responseElement.classList.remove("hiddenIt");
+  } else if (data.job === "") {
+    responseElement.innerHTML = "<p>Recuerda completar el campo 'Puesto'.</p>";
+    responseElement.classList.remove("hiddenIt");
+  } else if (data.photo === "") {
+    responseElement.innerHTML = "<p>Recuerda subir una imagen.</p>";
+    responseElement.classList.remove("hiddenIt");
+    /*} else if (data.phone === "") {
+    responseElement.innerHTML =
+      "<p>Recuerda completar el campo 'Puesto'</p>";
+    responseElement.classList.remove("hidden");*/
+  } else if (data.email === "") {
+    responseElement.innerHTML = "<p>Recuerda completar el campo 'Email'.</p>";
+    responseElement.classList.remove("hiddenIt");
+  } else if (pruebaemail(data.email)) {
+    responseElement.innerHTML =
+      "<p>Revisa tu email porque no es del todo correcto.</p>";
+    responseElement.classList.remove("hiddenIt");
+  } else if (data.linkedin === "") {
+    responseElement.innerHTML =
+      "<p>Recuerda completar el campo 'Linkedin'</p>.";
+    responseElement.classList.remove("hiddenIt");
+  } else if (data.github === "") {
+    responseElement.innerHTML = "<p>Recuerda completar el campo 'Github'.</p>";
+    responseElement.classList.remove("hiddenIt");
+  } else {
+    fetch("https://awesome-profile-cards.herokuapp.com/card", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success === false) {
+          responseElement.innerHTML = "<p>Revisa los campos sin completar</p>";
+          responseElement.classList.remove("hidden");
+        } else {
+          /*responseElement.innerHTML = `
+            <h3>La tarjeta ha sido creada:</h3>
+            <p><a href="${data.cardURL}">${data.cardURL}</a></p>`;*/
+          responseElement.classList.remove("hidden");
+          twitterContainer.classList.remove("js-hidden");
+        }
+      })
+      .catch(() => {
+        responseElement.innerHTML = `<p class="error">El servidor parece estar fuera de servicio. Paciencia. Inténtalo más tarde.</p>`;
+        responseElement.classList.remove("hidden");
+      });
+  }
+}
+
+createButton.addEventListener("click", handleClickCreate);
+
+function pruebaemail(valor) {
+  const re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  if (!re.exec(valor)) {
+    return true;
+  } else {
+    return false;
+  }
+}
