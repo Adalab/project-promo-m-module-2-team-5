fb97e98602c84155fe5b55028c23a610569896b2
jero10lf <jero10lf@gmail.com>
2021-05-27 13:44:00 +0200

    change localstorage

--- src/js/05_photo.js
+++ src/js/05_photo.js
@@ -1,11 +1,5 @@
 "use strict";
 
-const fr = new FileReader();
-const uploadBtn = document.querySelector(".js__profile-trigger");
-const fileField = document.querySelector(".js__profile-upload-btn");
-const profileImage = document.querySelector(".js__profile-image");
-const profilePreview = document.querySelector(".js__profile-preview");
-
 function getImage(e) {
   const myFile = e.currentTarget.files[0];
   fr.addEventListener("load", writeImage);
@@ -18,13 +12,20 @@ function writeImage() {
 }
 
 function updatePhoto() {
-  const currentPhoto = photo;
-  if (currentPhoto !== "") {
-    profilePreview.style.backgroundImage = `url(${currentPhoto})`;
-    profileImage.style.backgroundImage = `url(${currentPhoto})`;
-    data.photo = fr.result;
+  if (data.photo === "") {
+    profileImage.style.backgroundImage = ``;
+    profilePreview.style.backgroundImage = ``;
+  } else {
+    profileImage.style.backgroundImage = `url(${data.photo})`;
+    profilePreview.style.backgroundImage = `url(${data.photo})`;
   }
-  return data.photo;
+  // const currentPhoto = photo;
+  // if (currentPhoto !== "") {
+  //   profilePreview.style.backgroundImage = `url(${currentPhoto})`;
+  //   profileImage.style.backgroundImage = `url(${currentPhoto})`;
+  //   data.photo = fr.result;
+  // }
+  // return data.photo;
 }
 
 function clearPhoto() {
--- src/js/07_localStorage.js
+++ src/js/07_localStorage.js
@@ -22,19 +22,27 @@ function saveInLocalStorage() {
 function getFromLocalStorage() {
   const userDataInString = localStorage.getItem("data");
   if (userDataInString !== null) {
-    const data = JSON.parse(userDataInString);
-    document.querySelector(".js-name").value = data.name;
-    document.querySelector(".js-job").value = data.job;
-    document.querySelector(".js-email").value = data.email;
-    document.querySelector(".js-phone").value = data.phone;
-    document.querySelector(".js-linkedin").value = data.linkedin;
-    document.querySelector(".js-github").value = data.github;
-    //refreshData();
-    //photo = userData.photo;
-    // updatePhoto();
+    const dataLocal = JSON.parse(userDataInString);
+    document.querySelector(".js-name").value = dataLocal.name;
+    document.querySelector(".js-job").value = dataLocal.job;
+    document.querySelector(".js-email").value = dataLocal.email;
+    document.querySelector(".js-phone").value = dataLocal.phone;
+    document.querySelector(".js-linkedin").value = dataLocal.linkedin;
+    document.querySelector(".js-github").value = dataLocal.github;
+    data.palette = parseInt(dataLocal.palette);
+    data.name = dataLocal.name;
+    data.job = dataLocal.job;
+    data.email = dataLocal.email;
+    data.phone = dataLocal.phone;
+    data.linkedin = dataLocal.linkedin;
+    data.github = dataLocal.github;
+    data.photo = dataLocal.photo;
+    renderPreview(data);
+
+    updatePhoto();
     const paletteElements = document.querySelectorAll(".js-palette");
     for (const paletteElement of paletteElements) {
-      if (paletteElement.value === data.palette) {
+      if (parseInt(paletteElement.value) === dataLocal.palette) {
         paletteElement.checked = true;
       }
     }
